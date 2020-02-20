11292ea7a299d154967dd220e55793053a1960ed
NightForcee <60366012+NightForcee@users.noreply.github.com>
2020-02-20 23:01:33 +0300

    Initial commit

---
+++ src/com/company/DayDemo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class DayDemo {
+    int a;
+    int b;
+    int c;
+
+    public String formatString(int a, int b, int c) {
+        String output = a + "." + b + "." + c + "год.";
+        System.out.println(output);
+
+        return output;
+    }
+
+}
