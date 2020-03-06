f8df29180b0cf6cfec8a924ca26fbfc7ad11e7c6
NightForcee <60366012+NightForcee@users.noreply.github.com>
2020-03-06 21:40:28 +0300

    Lesson 5 task completed

--- src/lesson5/TestDemo.java
+++ src/lesson5/TestDemo.java
@@ -1,3 +1,15 @@
+/**
+ * Определить класс, который при инициализации принимает на
+ * вход 2 аргумента целого или вещественного типа. Задать
+ * перегруженные методы
+ * возвращающие результат арифметических
+ * операций над заданными полями и полями типа
+ * BigDecimal.
+ *
+ * @autour Slidenko Artem
+ * @version lesson5
+ */
+
 package lesson5;
 
 import java.math.BigDecimal;
@@ -16,6 +28,7 @@ public class TestDemo {
         this.int1 = arg1;
         this.int2 = arg2;
     }
+
     public TestDemo(int arg1, double arg2) {
         this.int1 = arg1;
         this.int2 = (int) arg2;
@@ -24,46 +37,56 @@ public class TestDemo {
         this.arg1 = new BigDecimal(arg1);
         this.arg2 = new BigDecimal(arg2);
     }
-    public TestDemo (double arg2,int arg1){
-        this(arg1,arg2);
+
+    public TestDemo(double arg2, int arg1) {
+        this(arg1, arg2);
     }
-    BigDecimal addAsBigDecimal (){
+
+    BigDecimal addAsBigDecimal() {
         return arg1.multiply(arg2);
     }
-    int addAsBigInteger(){
+
+    int addAsBigInteger() {
         return int1 + int2;
     }
-    int subtractionAsBigInteger(){
+
+    int subtractionAsBigInteger() {
         return int1 - int2;
     }
-    int multiplyAsBigInteger(){
+
+    int multiplyAsBigInteger() {
         return int1 * int2;
     }
-    int devideAsBigInteger(){
+
+    int devideAsBigInteger() {
         return int1 / int2;
     }
-    double addAsBigDouble(){
-        return  double1 + double2;
+
+    double addAsBigDouble() {
+        return double1 + double2;
     }
-    double subtractionAsBigDouble(){
-        return  double1 - double2;
+
+    double subtractionAsBigDouble() {
+        return double1 - double2;
     }
-    double multiplyAsBigDouble(){
-        return  double1 * double2;
+
+    double multiplyAsBigDouble() {
+        return double1 * double2;
     }
-    double devideAsBigDouble(){
-        return  double1 / double2;
+
+    double devideAsBigDouble() {
+        return double1 / double2;
     }
 
     public static void main(String[] args) {
-        TestDemo testInt = new TestDemo(50,23);
+        TestDemo testInt = new TestDemo(50, 23);
         System.out.println("Сложение типа int : " + testInt.addAsBigInteger());
         System.out.println("Вычитание типа int : " + testInt.subtractionAsBigInteger());
         System.out.println("Умножение типа int : " + testInt.multiplyAsBigInteger());
         System.out.println("Деление типа int : " + testInt.devideAsBigInteger());
         System.out.println("BigDecimal типа int : " + testInt.addAsBigDecimal());
 
-        TestDemo testDouble = new TestDemo(25,13.79);
+        TestDemo testDouble = new TestDemo(25, 13.79);
         System.out.println("Сложение типа double : " + testDouble.addAsBigDouble());
         System.out.println("Вычитание типа double : " + testDouble.subtractionAsBigDouble());
         System.out.println("Умножение типа double : " + testDouble.multiplyAsBigDouble());
