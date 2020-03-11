99861904e33df68bd7d5e41cbaf129355fc3a232
NightForcee <60366012+NightForcee@users.noreply.github.com>
2020-03-11 16:39:46 +0300

    Main - have Javadoc + log4j.properties

--- src/lesson8/Main.java
+++ src/lesson8/Main.java
@@ -8,10 +8,19 @@
 
 package lesson8;
 
+import org.apache.log4j.Logger;
+
 public class Main extends ShapeUtils {
+    public static final Logger LOGGER = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         Circle circle = new Circle(5);
         System.out.println(circle.getPerimeter());
         System.out.println(circle.getSquare());
+
+        LOGGER.trace("Before call say method");
+        LOGGER.debug("Before call say method");
+        LOGGER.info("Before call say method");
+        LOGGER.warn("Before call say method");
     }
 }
